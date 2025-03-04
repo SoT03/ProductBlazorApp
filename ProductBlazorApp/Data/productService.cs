72816c5589b47e81b7fb9424be8ed9e2c96aef1e
SoT03 <bogdan.zygadlo123@gmail.com>
2025-03-04 10:33:11 +0100

    Added async function for fetching products and created home page basic structure

---
+++ ProductBlazorApp/Data/productService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductBlazorApp.Models;
+
+namespace ProductBlazorApp.Data
+{
+    public class ProductService(ProductContext context)
+    {
+        private readonly ProductContext _context = context;
+
+        
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
