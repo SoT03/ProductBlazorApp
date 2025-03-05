704063285f2e71843dd6d0adfa6a143ba6db5c10
SoT03 <bogdan.zygadlo123@gmail.com>
2025-03-05 18:46:28 +0100

    Added styling

---
+++ ProductBlazorApp/Components/UI/ConfirmDeleteModal.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+
+namespace ProductBlazorApp.Components.UI
+{
+    public partial class ConfirmDeleteModal
+    {
+        [Parameter] public string ProductName { get; set; } = string.Empty;
+        [Parameter] public EventCallback OnConfirm { get; set; }
+        [Parameter] public EventCallback OnCancel { get; set; }
+
+
+        private async Task OnConfirmDelete()
+        {
+            await OnConfirm.InvokeAsync();
+        }
+
+        private async Task HandleBackdropClick(MouseEventArgs e)
+        {
+
+            await OnCancel.InvokeAsync();
+        }
+    }
+}
